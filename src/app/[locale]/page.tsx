a83e2e2d4b1ceb0e8324221247d4204e0ca37dc7
MatiasJRB <40368281+MatiasJRB@users.noreply.github.com>
2025-05-02 13:16:53 -0300

    fix: footer

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,6 @@
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -12,7 +11,6 @@ export default function Home() {
       <Hero />
       <Services />
       <Contact />
-      <Footer />
     </div>
   );
 }
