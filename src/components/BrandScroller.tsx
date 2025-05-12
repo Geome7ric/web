4c33df0804a205b4bb04ad8f0aee4197fb072155
MatiasJRB <40368281+MatiasJRB@users.noreply.github.com>
2025-05-12 19:10:03 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/BrandScroller.tsx
+++ src/components/BrandScroller.tsx
@@ -2,53 +2,7 @@
 
 import React from "react";
 import InfiniteScroll from "./InfiniteScroll";
-
-// Define the assets to be displayed in the scroller
-let BRAND_ASSETS = [
-  {
-    id: "calendar",
-    src: "/assets/views/calendar.webp",
-    alt: "Calendar logo",
-    width: 180,
-    height: 60,
-  },
-  {
-    id: "app",
-    src: "/assets/views/app.webp",
-    alt: "App logo",
-    width: 200,
-    height: 70,
-  },
-  {
-    id: "calendar-extended",
-    src: "/assets/views/calendar-extended.webp",
-    alt: "Calendar extended logo",
-    width: 220,
-    height: 70,
-  },
-  {
-    id: "two-apps",
-    src: "/assets/views/two-apps.webp",
-    alt: "Two apps logo",
-    width: 220,
-    height: 70,
-  },
-
-  {
-    id: "tablet",
-    src: "/assets/views/tablet.webp",
-    alt: "Tablet logo",
-    width: 220,
-    height: 70,
-  },
-];
-
-BRAND_ASSETS = BRAND_ASSETS.concat(
-  BRAND_ASSETS.map((asset) => ({
-    ...asset,
-    id: `${asset.id}-copy`,
-  }))
-);
+import { useEffect, useState } from "react";
 
 interface BrandScrollerProps {
   className?: string;
@@ -56,12 +10,79 @@ interface BrandScrollerProps {
 }
 
 const BrandScroller: React.FC<BrandScrollerProps> = ({ className = "" }) => {
+  // use effect para determinar si estamos en modo claro o modo oscuro
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
+    setIsDarkMode(matchMedia.matches);
+
+    const handleChange = (event: MediaQueryListEvent) => {
+      setIsDarkMode(event.matches);
+    };
+
+    matchMedia.addEventListener("change", handleChange);
+
+    return () => {
+      matchMedia.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  const assetPrefix = isDarkMode ? "dark" : "light";
+  // Define the assets to be displayed in the scroller
+  const brandAssets = [
+    {
+      id: "calendar",
+      src: `/assets/views/${assetPrefix}/calendar.webp`,
+      alt: "Calendar logo",
+      width: 180,
+      height: 60,
+    },
+    {
+      id: "app",
+      src: `/assets/views/${assetPrefix}/app.webp`,
+      alt: "App logo",
+      width: 200,
+      height: 70,
+    },
+    {
+      id: "calendar-extended",
+      src: `/assets/views/${assetPrefix}/calendar-extended.webp`,
+      alt: "Calendar extended logo",
+      width: 220,
+      height: 70,
+    },
+    {
+      id: "two-apps",
+      src: `/assets/views/${assetPrefix}/two-apps.webp`,
+      alt: "Two apps logo",
+      width: 220,
+      height: 70,
+    },
+    {
+      id: "tablet",
+      src: `/assets/views/${assetPrefix}/tablet.webp`,
+      alt: "Tablet logo",
+      width: 220,
+      height: 70,
+    },
+  ];
+
+  // Create duplicate assets with unique IDs
+  const allAssets = [
+    ...brandAssets,
+    ...brandAssets.map((asset) => ({
+      ...asset,
+      id: `${asset.id}-copy`,
+    })),
+  ];
+
   return (
-    <section className={`w-full  ${className}`} id="brand-scroller">
-      <div className=" flex flex-col gap-32 w-full">
+    <section className={`w-full ${className}`} id="brand-scroller">
+      <div className="flex flex-col gap-32 w-full">
         {/* First slider with ltr (left-to-right) direction */}
-        <div className="min-h-[150px] ">
-          <InfiniteScroll assets={BRAND_ASSETS} />
+        <div className="min-h-[150px]">
+          <InfiniteScroll assets={allAssets} />
         </div>
       </div>
     </section>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 // import Link from "next/link";
 
 const Services = () => {
@@ -9,12 +10,15 @@ const Services = () => {
   const sections = [
     {
       key: "customApps",
+      image: "/assets/services/app_perfect_fit.webp",
     },
     {
       key: "operationalTools",
+      image: "/assets/services/bussiness_solutions.webp",
     },
     {
       key: "automation",
+      image: "/assets/services/automatic_reports.webp",
     },
   ];
 
@@ -30,21 +34,46 @@ const Services = () => {
         </h2>
         <p className="max-w-5xl mx-auto text-lg md:text-base text-center text-black/80 dark:text-white/80 mb-16">
           {t("intro")}
-        </p>
-
+        </p>{" "}
         {/* Secciones */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-12 my-24">
-          {sections.map(({ key }) => (
+          {sections.map(({ key, image }) => (
             <div
               key={key}
-              className="bg-white/50 dark:bg-black/20 rounded-2xl p-6 shadow-md hover:shadow-lg transition-all duration-300 flex flex-col"
+              className="bg-white/50
+               dark:bg-black/20 
+               rounded-2xl  shadow-md hover:shadow-lg 
+               transition-all duration-300 flex flex-col"
             >
-              <h3 className="text-xl md:text-lg mb-4 text-center text-black dark:text-white">
-                {t(`sections.${key}.title`)}
-              </h3>
+              <div className="row flex justify-around items-center  p-4">
+                {/* <Image
+                  src={image}
+                  alt={t(`sections.${key}.title`)}
+                  width={80}
+                  height={80}
+                  className="rounded-lg"
+                /> */}
 
-              <div className="mt-2 mb-4">
-                <p className="font-bold text-accent mb-1">
+                <h3
+                  className="text-xl md:text-lg 
+                  text-left flex items-center
+                  text-black dark:text-white"
+                >
+                  {t(`sections.${key}.title`).replace(/^\💻\s|\⚙️\s|\📊\s/, "")}
+                </h3>
+              </div>
+
+              {/* un separador de color que ocupe todo el ancho */}
+              <div
+                className="h-1 bg-gradient-to-r
+               dark:from-accent dark:via-accent/50 dark:to-accent/20
+               from-accent/50 via-accent/20 to-accent/10
+               
+               w-full mb-2"
+              ></div>
+
+              <div className="mt-2 mb-4 px-4">
+                <p className="font-bold dark:text-accent mb-1">
                   {t(`sections.${key}.what`)}
                 </p>
                 <p className="text-black/70 dark:text-white/70 leading-relaxed md:text-sm">
@@ -52,8 +81,8 @@ const Services = () => {
                 </p>
               </div>
 
-              <div>
-                <p className="font-bold text-accent mb-1">
+              <div className="px-4 pb-4">
+                <p className="font-bold dark:text-accent mb-1">
                   {t(`sections.${key}.how`)}
                 </p>
                 <p className="text-black/70 dark:text-white/70 leading-relaxed md:text-sm">
@@ -63,7 +92,6 @@ const Services = () => {
             </div>
           ))}
         </div>
-
         {/* Por qué elegirnos */}
         {/* <div className="mt-24 text-center">
           <h3 className="text-subtitle  mb-6">{t("whyTitle")}</h3>
@@ -78,7 +106,6 @@ const Services = () => {
             ))}
           </ul>
         </div> */}
-
         <div className="mt-24 text-center">
           {/* <Link
             className="px-6 
